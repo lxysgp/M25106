a41e92447cbf5d0cba5effbf7a45ff0d08151f19
name <11pro12ispro@gmail.com>
2025-03-22 16:03:11 +0800

    just asking why do we need a js file to make a list

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 #template
 // Example classmates list
 const classmates = ["Xinyuan", "linteng", "caden", "yusu", "zhengxian"];
-
 const listElement = document.getElementById("class-list");
-
 classmates.forEach(name => {
   const li = document.createElement("li");
   li.textContent = name;
