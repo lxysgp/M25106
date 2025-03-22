577fee1cdd3768c891a172219f166d835065c8ac
lxysgp <68497851+lxysgp@users.noreply.github.com>
2025-03-22 15:43:50 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+#template
+// Example classmates list
+const classmates = ["Xinyuan", "linteng", "caden", "yusu", "zhengxian"];
+
+const listElement = document.getElementById("class-list");
+
+classmates.forEach(name => {
+  const li = document.createElement("li");
+  li.textContent = name;
+  listElement.appendChild(li);
+});
